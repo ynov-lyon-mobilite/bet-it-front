38c920f02243a00c1f948b57151208d3b40b29f3
johannadznd <johanna.dezarnaud@ynov.com>
2020-11-26 10:51:18 +0100

    début page home

--- src/components/Home/MatchList.vue
+++ src/components/Home/MatchList.vue
@@ -1,13 +1,39 @@
 <template>
-  <v-container class="vh-100 matches-container">
-    <v-row class="flex-row justify-center">
+  <v-container class="vh-100 matches-container" style="margin-top: -10vw;">
+    <v-row id="title"  class="ml-5 mb-10 flex-row justify-space-between">
       <div>
-        <h2>Worlds 2020</h2>
+        <h2 style="text-shadow:0 0 10px black, 0 0 20px #454d7e, 0 0 30px #454d7e, 0 0 70px #454d7e">WORLDS 2020</h2>
       </div>
-    </v-row>
-    <v-row class="flex-row justify-center">
+      <div>
+        <img style="width: 10vw;
+    margin-top: -2vw;" src="../../assets/betties.png" alt="monnaie">
+      </div>
+      </v-row>
+      
+    <v-row class="flex-row justify-center" style="margin: 0 0 -3vw 21vw;">
+      
       <v-card
-          v-for="n in 5"
+          @click="overlay = !overlay"
+          color="white"
+          cols="3"
+          class="ma-1 d-flex justify-center "
+          width="200"
+          height="200"
+          outlined
+          
+      >
+        <div class="d-flex align-center div-text justify-space-between">
+          <v-img src="../../assets/DW_Logo.png" width="80" class="mr-5" ></v-img>
+          <div class="line"></div>
+          <v-img src="../../assets/100T_logo.png" width="80" class="ml-5" ></v-img>
+        </div>
+        
+      </v-card>
+    </v-row>
+    <v-row class="flex-row justify-center" style="margin-bottom: 0;">
+      <v-card
+          @click="overlay = !overlay"
+          v-for="n in 3"
           color="white"
           :key="n"
           cols="3"
@@ -15,84 +41,93 @@
           width="200"
           height="200"
           outlined
-      >
-        <div class="d-flex align-center div-text w-100 justify-space-between">
-          <div class="text-card">team</div>
-          <div class="line"></div>
-          <div  class="text-card">team</div>
-        </div>
-      </v-card>
-    </v-row>
-    <v-row class="flex-row justify-center">
-      <div>
-        <h2>Compétitions 2020</h2>
-      </div>
-    </v-row>
-    <v-row class="flex-row justify-center">
-      <v-card
-          v-for="n in 5"
-          color="white"
-          :key="n"
-          cols="3"
-          class="ma-1 d-flex justify-center"
-          width="200"
-          height="200"
-          outlined
-      >
-        <div class="d-flex align-center div-text w-100 justify-space-between">
-          <div class="text-card">team</div>
-          <div class="line"></div>
-          <div  class="text-card">team</div>
-        </div>
-      </v-card>
-    </v-row>
 
-    <v-row class="flex-row justify-center">
-      <div>
-        <h2>Les matchs OSEF 2020</h2>
-      </div>
+      >
+        <div class="d-flex align-center div-text justify-space-between">
+          <v-img src="../../assets/DW_Logo.png" width="80" class="mr-5" ></v-img>
+          <div class="line"></div>
+          <v-img src="../../assets/100T_logo.png" width="80" class="ml-5" ></v-img>
+        </div>
+      </v-card>
     </v-row>
-  <v-row  class="flex-row justify-center ">
-    <v-card
-        v-for="n in 15"
-        color="white"
-        :key="n"
-        cols="3"
-        class="ma-1 d-flex justify-center"
-        width="200"
-        height="200"
-        outlined
-    >
-      <div class="d-flex align-center div-text w-100 justify-space-between">
-        <div class="text-card">team</div>
-        <div class="line"></div>
-        <div  class="text-card">team</div>
-      </div>
-    </v-card>
-  </v-row>
-    <v-row class="flex-row justify-center">
-      <div>
-        <h2>Les trucs paumés 2020</h2>
-      </div>
+      <v-row class="flex-row justify-start " style="width: 50vw;margin-bottom: 0;margin-left: 5vw;">
+      <v-card
+          @click="overlay = !overlay"
+          v-for="n in 3"
+          color="white"
+          :key="n"
+          cols="3"
+          class="ma-1 d-flex justify-center "
+          width="200"
+          height="200"
+          outlined
+      >
+        <div class="d-flex align-center div-text justify-space-between">
+          <v-img src="../../assets/DW_Logo.png" width="80" class="mr-5" ></v-img>
+          <div class="line"></div>
+          <v-img src="../../assets/100T_logo.png" width="80" class="ml-5" ></v-img>
+        </div>
+      </v-card>
     </v-row>
-    <v-row  class="flex-row justify-center ">
-    <v-card
-        v-for="n in 3"
-        color="white"
-        :key="n"
-        cols="3"
-        class="ma-1 d-flex justify-center"
-        width="200"
-        height="200"
-        outlined
+    <v-row class="flex-row justify-start " style="width: 20vw; margin-left: 8vw; margin-top: -4vw;">
+      
+      <v-card
+          @click="overlay = !overlay"
+          color="white"
+          cols="3"
+          class="ma-1 d-flex justify-center "
+          width="200"
+          height="200"
+          outlined
+      >
+        <div class="d-flex align-center div-text justify-space-between">
+          <v-img src="../../assets/DW_Logo.png" width="80" class="mr-5" ></v-img>
+          <div class="line"></div>
+          <v-img src="../../assets/100T_logo.png" width="80" class="ml-5" ></v-img>
+        </div>
+        
+      </v-card>
+    </v-row>
+    <v-overlay
+      :z-index="zIndex"
+      :value="overlay"
+      :opacity="0.8"
     >
-      <div class="d-flex align-center div-text w-100 justify-space-between">
-        <div class="text-card">team</div>
-        <div class="line"></div>
-        <div  class="text-card">team</div>
-      </div>
-    </v-card>
-  </v-row>
+    <v-row class="flex-row justify-center">
+      
+      <v-card
+          color="grey"
+          cols="3"
+          class="ma-1 d-flex justify-center "
+          width="300"
+          height="300"
+          outlined
+          id="test"          
+      >
+        <div class="d-flex align-center div-text justify-space-between">
+          <div>
+               <v-img src="../../assets/DW_Logo.png" width="80" class="mr-5" ></v-img>
+            <v-btn
+  elevation="10"
+>test</v-btn>
+          </div>
+         
+          <div class="line"></div>
+          <v-img src="../../assets/100T_logo.png" width="80" class="ml-5" ></v-img>
+        </div>
+        
+      </v-card>
+    </v-row>
+      <v-btn
+
+        class="white--text"  
+        color="teal"
+        @click="overlay = false"
+      >
+        Hide Overlay
+      </v-btn>
+    </v-overlay>
+  
   </v-container>
 
 </template>
@@ -102,6 +137,10 @@ export default {
   name: "MatchList",
   data: () => ({
     matches: [],
+    overlay: false,
+    zIndex: 0,
+    opacity: 0.7,
+    hover : false,
   }),
 }
 </script>
@@ -109,6 +148,16 @@ export default {
 <style lang="scss" scoped>
 
 .matches-container{
+
+
+  #test:hover{background: unset;
+  transition: none;
+    color: grey;}
+
+  #title{
+        transform: rotate(0deg);
+        font-size: 30px;
+  }
   .row{
     position: relative;
     top : 200px;
@@ -139,8 +188,6 @@ export default {
   }
 }
 
-.rotate{
-  transform: rotate(-45deg);
-}
+
 
 </style>
