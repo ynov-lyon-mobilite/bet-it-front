d1d109cfaa85317faa38b2c778c2c9510d6ff55d
johannadznd <johanna.dezarnaud@ynov.com>
2020-12-16 18:13:39 +0100

    fix : change color

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -88,6 +88,7 @@
           :close-on-content-click="false"
           :activator="selectedElement"
           offset-x
+          
         >
           <v-card
             color="grey lighten-4"
@@ -98,30 +99,33 @@
               :color="selectedEvent.color"
               dark
             >
-              <v-btn icon>
-                <v-icon>mdi-pencil</v-icon>
-              </v-btn>
               <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
-              <v-spacer></v-spacer>
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn>
+            
             </v-toolbar>
-            <v-card-text>
+            <v-card-text >
               <span v-html="selectedEvent.details"></span>
             </v-card-text>
+            <v-row>
             <v-card-actions>
               <v-btn
                 text
-                color="secondary"
+                color="primary"
+               
+              >
+            Accéder au paris
+              </v-btn>
+            </v-card-actions>
+            <v-card-actions>
+              <v-btn
+                text
+                color="primary"
                 @click="selectedOpen = false"
               >
                 Cancel
               </v-btn>
             </v-card-actions>
+            </v-row>
+        
           </v-card>
         </v-menu>
       </v-sheet>
