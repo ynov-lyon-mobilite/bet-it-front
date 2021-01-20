8ce2360961f976d5cf65ea0b1ba225e8ae909155
dryisf <daryl.issoufi@gmail.com>
2021-01-20 17:03:17 +0100

    fix(landing): hide navbar, redirect to home

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,22 +1,29 @@
 <template>
-  <div>
-    <video class="vw-100 vh-100" autoplay loop playsinline muted>
-      <source src="../assets/video.webm" type="video/webm" />
-      <source src="../assets/video.mp4" type="video/mp4" />
-    </video>
-    <div
-      class="logo-wrapper text-center vh-100 d-flex align-center justify-center"
-    >
-      <div class="col-6">
-        <v-img class="" src="../assets/logo.svg"></v-img>
+  <v-fade-transition>
+    <div>
+      <video class="vw-100 vh-100" autoplay loop playsinline muted>
+        <source src="../assets/video.webm" type="video/webm" />
+        <source src="../assets/video.mp4" type="video/mp4" />
+      </video>
+      <div
+        class="logo-wrapper text-center vh-100 d-flex align-center justify-center"
+      >
+        <div class="col-6">
+          <v-img class="" src="../assets/logo.svg"></v-img>
+        </div>
       </div>
     </div>
-  </div>
+  </v-fade-transition>
 </template>
 
 <script>
 export default {
-  name: "Landing"
+  name: "Landing",
+  mounted() {
+    setTimeout(() => {
+      this.$router.push({ path: "/home" });
+    }, 3000);
+  },
 };
 </script>
 
